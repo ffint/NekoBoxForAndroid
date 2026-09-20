1fd3d8903c6dba2d70af47a586e96d9bf42e8aba
ffint <hfpqlipvlgb6p4@hotmail.com>
2026-09-20 14:36:45 +0800

    build: remove unused Roomigrant processor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,8 +81,6 @@ dependencies {
     implementation("androidx.room:room-runtime:2.7.2")
     ksp("androidx.room:room-compiler:2.7.2")
     implementation("androidx.room:room-ktx:2.7.2")
-    implementation("com.github.MatrixDev.Roomigrant:RoomigrantLib:0.3.4")
-    ksp("com.github.MatrixDev.Roomigrant:RoomigrantCompiler:0.3.4")
 
     testImplementation("junit:junit:4.13.2")
 
